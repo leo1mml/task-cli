10671905e5f62108afaae930371e796dc5f2ec2c
Leonel Lima <llima.smo@eos-uptrade.de>
2025-07-14 13:21:07 +0100

    Add base impl for todo list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use clap::{Parser, Subcommand, ValueEnum};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Add {
+        #[arg(short, long)]
+        status: TaskStatus,
+        #[arg(short, long)]
+        description: String,
+    },
+    Delete,
+    Update,
+    List,
+}
+
+#[derive(Debug)]
+struct Task {
+    status: TaskStatus,
+    description: String,
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum TaskStatus {
+    Todo,
+    InProgress,
+    Done,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.command {
+        Command::Add {
+            status,
+            description,
+        } => todo!(),
+        Command::Delete => todo!(),
+        Command::Update => todo!(),
+        Command::List => todo!(),
+    }
 }
