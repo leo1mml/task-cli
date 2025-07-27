cce05d3592a67d2d03dcde1f7e2d17ddd3fc2936
Leonel Lima <mml_leo@hotmail.com>
2025-07-27 23:59:23 +0100

    Listen for single presses correctly

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use anyhow::Error;
 use clap::{Parser, Subcommand, ValueEnum};
 use crossterm::{
     event::{Event, KeyCode, KeyEvent, KeyEventKind, read},
-    terminal::{disable_raw_mode, enable_raw_mode},
+    terminal::{disable_raw_mode, enable_raw_mode, is_raw_mode_enabled},
 };
 use serde::{Deserialize, Serialize};
 use std::io::stdin;
@@ -20,17 +20,8 @@ pub struct Cli {
 impl CliInteraction for Cli {
     fn loop_for_commands(&self) {
         loop {
-            clear_and_reset();
-            present_commands_prompt();
-            match read() {
-                Ok(Event::Key(key_event)) => {
-                    let Some(command) = self.command_for_event(key_event) else {
-                        continue;
-                    };
-                    let _ = self.run_command(&command);
-                }
-                _ => eprintln!("Error when reading event."),
-            }
+            self.read_command()
+                .expect("Could not read the given command");
         }
     }
 
@@ -51,6 +42,24 @@ impl CliInteraction for Cli {
 }
 
 impl Cli {
+    fn read_command(&self) -> Result<(), Error> {
+        clear_and_reset();
+        present_commands_prompt();
+        loop {
+            enable_raw_mode()?;
+            match read() {
+                Ok(Event::Key(key_event)) => {
+                    let Some(command) = self.command_for_event(key_event) else {
+                        break;
+                    };
+                    let _ = self.run_command(&command);
+                }
+                _ => eprintln!("Error when reading event."),
+            }
+            disable_raw_mode()?;
+        }
+        Ok(())
+    }
     fn command_for_event(&self, event: KeyEvent) -> Option<Command> {
         match event.code {
             KeyCode::Char('a') => self.make_add_command(),
@@ -86,7 +95,9 @@ impl Cli {
 pub fn wait_for_any_key() -> Result<(), Error> {
     // Enable raw mode to get immediate key presses.
     // This also disables echoing characters and special terminal processing.
-    enable_raw_mode()?;
+    if !is_raw_mode_enabled()? {
+        enable_raw_mode()?;
+    }
 
     println!("\nPress any key to continue...");
 
